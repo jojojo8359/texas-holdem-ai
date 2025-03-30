a59e572caa93e84ee7474a214c7562e08a0935dd
jojojo8359 <31038772+jojojo8359@users.noreply.github.com>
2025-03-30 18:19:29 -0400

    Switch from manual evaluator to library

--- main.py
+++ main.py
@@ -3,7 +3,7 @@ from util import rank_hand
 
 if __name__ == "__main__":
     # Royal Flush - 7 cards
-    # hand = ["Ah", "Th", "Kh", "Qh", "Jh", "3s", "6h"]
+    hand = ["Ah", "Th", "Kh", "Qh", "Jh", "3s", "6h"]
 
     # Royal Flush - 5 cards
     # hand = ["Ah", "Th", "Kh", "Qh", "Jh"]
@@ -21,11 +21,21 @@ if __name__ == "__main__":
     # hand = ["3c", "4h", "5d", "6c", "7s"]
     # hand = ["Ac", "2h", "3d", "4c", "5s"]
 
-    hand = ["Js", "3h", "6d", "5d", "6s", "7s", "7c"]
-    rank_hand(hand)
+    # hand = ["Js", "3h", "6d", "5d", "6s", "7s", "7c"]
+    # rank_hand(hand)
 
-    hand = ["Ad", "Td", "6d", "5d", "6s", "7s", "7c"]
-    rank_hand(hand)
+    # hand = ["Ad", "Td", "6d", "5d", "6s", "7s", "7c"]
+    # rank_hand(hand)
 
-    hand = ["9s", "Ah", "6d", "5d", "6s", "8s", "8c"]
-    rank_hand(hand)
+    # hand = ["9s", "Ah", "6d", "5d", "6s", "8s", "8c"]
+    # hand = ["Ad", "2d", "3d", "4d", "5d", "7d", "9d"]
+    # hand = ["2d", "2c", "2h", "2s", "3d"]
+    # hand = ["2d", "2c", "2h", "3s", "3d"]
+    # hand = ["Ad", "Kd", "Qd", "Jd", "9d"]
+    # hand = ["2d", "3d", "4d", "5d", "7d"]
+    # hand = ["2d", "3h", "4c", "5s", "As"]
+    # hand = ["As", "Ad", "Ac", "Ks", "Qd"]
+    # hand = ["2s", "2d", "2c", "3s", "4d"]
+    # hand = ["As", "Ad", "Kc", "Ks", "Qd"]
+    # hand = ["2s", "2d", "3c", "3s", "4d"]
+    print(rank_hand(hand))
